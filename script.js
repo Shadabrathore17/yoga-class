2d18180661ef102f644c8c5a36e56c8babb159f9
Shadab rathore <shadab.22rathod@gmail.com>
2024-11-30 20:12:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showDetails(exerciseId) {
+    const details = document.getElementById(exerciseId);
+    if (details.style.display === "none" || details.style.display === "") {
+        details.style.display = "block";
+    } else {
+        details.style.display = "none";
+    }
+}
